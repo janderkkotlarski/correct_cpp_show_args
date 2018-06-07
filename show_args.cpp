422ecf4e87bc3873c7740c0b95542c711dec531a
janderkkotlarski <j.d.kotlarski@gmx.com>
2018-06-07 20:51:02 +0200

    first sync, then turn on travis, then push

--- show_args.cpp
+++ show_args.cpp
@@ -9,6 +9,7 @@ void CoutVector(const std::vector<T>& v)
   std::copy(std::begin(v), std::end(v), std::ostream_iterator<T>(std::cout," "));
 }
 
+
 int main(int argc, char* argv[])
 {
   const std::vector<std::string> args(argv, argv + argc);
