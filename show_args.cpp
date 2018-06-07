426d4e5c16b0ffdfc583498c5bb36bcf1a47b164
janderkkotlarski <j.d.kotlarski@gmx.com>
2018-06-07 21:01:57 +0200

    forgot documentation

--- show_args.cpp
+++ show_args.cpp
@@ -3,13 +3,14 @@
 #include <vector>
 #include <iterator>
 
+/// auto-cout-ing a vector of cout-able variables using template classes
 template <class T>
 void CoutVector(const std::vector<T>& v)
 {
   std::copy(std::begin(v), std::end(v), std::ostream_iterator<T>(std::cout," "));
 }
 
-
+/// main function with argc inputs of argv
 int main(int argc, char* argv[])
 {
   const std::vector<std::string> args(argv, argv + argc);
