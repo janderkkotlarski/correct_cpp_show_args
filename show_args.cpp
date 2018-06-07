815e6c9cdfb8ff9af6a48df3823fe645665d3b46
janderkkotlarski <j.d.kotlarski@gmx.com>
2018-06-07 20:46:42 +0200

    might get this in one push

---
+++ show_args.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iterator>
+
+template <class T>
+void CoutVector(const std::vector<T>& v)
+{
+  std::copy(std::begin(v), std::end(v), std::ostream_iterator<T>(std::cout," "));
+}
+
+int main(int argc, char* argv[])
+{
+  const std::vector<std::string> args(argv, argv + argc);
+
+  CoutVector(args);
+
+  std::cout << "\n";
+}
